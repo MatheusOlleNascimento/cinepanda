f15ee6763af643015f710758d0fdf9335834fa0a
Matheus <matheusollenascimento@gmail.com>
2024-10-17 22:20:25 -0300

    fix: recude loading time

--- lib/views/splash.dart
+++ lib/views/splash.dart
@@ -15,7 +15,7 @@ class SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 8), () {
+    Future.delayed(const Duration(seconds: 5), () {
       // ignore: use_build_context_synchronously
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(builder: (context) => const HomePage()),
